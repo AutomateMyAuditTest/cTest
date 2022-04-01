98567608000570ffea91d37268647db16565ea50
Mark Wagner <>
2022-04-01 19:37:01 -0400

    fixed comment in functions script

--- scripts/functions.py
+++ scripts/functions.py
@@ -2,7 +2,7 @@
 def logcmd(current_path, cmd):
     '''
     Creates and appends a file to log which tests were run.
-    Used by tests.py and testplug.py.
+    Used by tests.py.
     '''
     f2 = open(current_path + '/_builds/tests_run.txt', 'a')
     f2.write('\n' + cmd + '\n')
